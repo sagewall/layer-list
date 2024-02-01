26132c5ec7c5113c940c84038f2f68a3a7a2015e
Sage Wall <swall@esri.com>
2024-02-01 11:58:46 -0700

    chore: update layer effect to add saturate

--- src/main.ts
+++ src/main.ts
@@ -130,20 +130,7 @@ view.when(() => {
     added.forEach((item) => {
       const { layer } = item;
       if (layer instanceof FeatureLayer) {
-        layer.effect = [
-          {
-            scale: 36978595,
-            value: "drop-shadow(3px, 3px, 4px)",
-          },
-          {
-            scale: 18489297,
-            value: "drop-shadow(2px, 2px, 3px)",
-          },
-          {
-            scale: 4622324,
-            value: "drop-shadow(1px, 1px, 2px)",
-          },
-        ];
+        layer.effect = "drop-shadow(2px, 2px, 3px) saturate(250%)";
       }
     });
   });
